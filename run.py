381da16e7288949e960d51e3d00cc9687e983179
leo <1336438003@qq.com>
2024-06-24 20:40:30 +0800

    all加筛选

--- run.py
+++ run.py
@@ -2,8 +2,7 @@ from open_requests import open_http
 from bs4 import BeautifulSoup
 url="https://www.douguo.com"
 f_rest=open_http(url)
-print(f_rest)
-html_doc = f_rest.text
-soup = BeautifulSoup(html_doc,"lxml")
-recipe_img = soup.select("imublo clearfix")
-print(recipe_img)
+# print(f_rest)
+soup = BeautifulSoup(f_rest,"lxml")
+recipe_material = soup.findAll("div", {"class":"imublo clearfix"})
+print(recipe_material)
